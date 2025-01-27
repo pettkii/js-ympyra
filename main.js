19908d46c7eb371f9462f316b49404ec64bf2580
Petteri Kiiskinen <petteri.kiiskinen@edu.salpaus.fi>
2025-01-27 13:32:06 +0200

    update

--- main.js
+++ main.js
@@ -1,7 +1,40 @@
 // get user input
-let halkaisija = prompt("Syötä halkaisija(cm): ");
+/*let halkaisija = prompt("Syötä halkaisija(cm): ");
 console.log("Halkaisija:" + halkaisija);
+*/
 
+// calculate and return circumference
+function laskeym(a){
+    return Math.PI * a;
+}
+
+// calculate and returnr area
+function laskepa(a){
+    return Math.PI * ((a/2)**2);
+}
+// calculates circumference and area for a given value
+function ympa(halkaisija){
+    console.log(halkaisija);
+
+    let ympm = laskeym(halkaisija);
+
+    console.log(ympm);
+
+    let pa = laskepa(halkaisija);
+
+    console.log(pa);
+
+    const a = document.createElement('p');
+    a.setAttribute('innerHTML', ympm);
+    const b = document.createElement('p');
+    b.setAttribute('innerHTML', pa);
+
+    document.body.appendChild(a);
+    document.body.appendChild(b);
+}
+
+
+/*
 // calculate circumference
 let ympm = Math.PI * halkaisija;
 
@@ -11,3 +44,4 @@ console.log(ympm);
 let pa = Math.PI * ((halkaisija/2)**2);
 
 console.log(pa);
+*/
